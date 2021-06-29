3831aa13b96a03ed74bfca03755d0d37dda623f0
Sanjish Maharjan <sanjishmaharjan619@gmail.com>
2021-06-29 12:58:02 +0545

    Familiarization with friend class

--- Assignments/familiarization_with_friend_class/friend_function.cpp
+++ Assignments/familiarization_with_friend_class/friend_function.cpp
@@ -1,32 +1,33 @@
 /*Wap to create class Sample with 2 float data members and 
 member functions void getdata()to input the values and one 
 friend function float mean(Sample)to calculate and return mean value*/
-//WAP to add private data of two different classes.
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 class Sample
 {
-  private :
-  double num1,num2;
+private:
+  double num1, num2;
 
-  public :
-  // Declaring the friend function so it can access the private data of class  
-  friend double setmean(Sample S);  
-  void getdata(double a,double b){
-      num1=a;
-      num2=b;
-      cout<<"The data you have set is "<<a << ","<<b<<endl;
-  } 
+public:
+  // Declaring the friend function so it can access the private data of class
+  friend double setmean(Sample S);
+  void getdata(double a, double b)
+  {
+    num1 = a;
+    num2 = b;
+    cout << "The data you have set is " << a << "," << b << endl;
+  }
 };
-double setmean(Sample S){
-      return((S.num1+S.num2)/2);
-  } 
+double setmean(Sample S)
+{
+  return ((S.num1 + S.num2) / 2);
+}
 int main()
 {
-    Sample S1;
-    S1.getdata(4.3,5.7);
-    cout<<"Mean is :"<<setmean(S1)<<endl;
+  Sample S1;
+  S1.getdata(4.3, 5.7);
+  cout << "Mean is :" << setmean(S1) << endl;
 
-return 0;
+  return 0;
 }
