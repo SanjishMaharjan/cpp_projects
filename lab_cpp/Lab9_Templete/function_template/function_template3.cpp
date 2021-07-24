e650ee4162d433a8648ce3ed1490f43e17ee346a
Sanjish Maharjan <sanjishmaharjan619@gmail.com>
2021-07-24 18:55:49 +0545

    adding a folder function template

---
+++ lab_cpp/Lab9_Templete/function_template/function_template3.cpp
@@ -0,0 +1,24 @@
+//Illustration of precedence for implicit datatype or explicit datatype
+#include <iostream>
+using namespace std;
+
+template <class T1, class T2>
+T1 testfunc(T2 a) //template function
+{
+    T1 b;
+    b = static_cast<T1>(a);
+    cout << b << endl;
+}
+
+int main()
+{
+    int inum = 5;
+
+    testfunc<float>(inum);
+    //here, T1 will be float and T2 will be integer.
+    testfunc<int, float>(inum);
+    //here,T1 will be integer and T2 will be float.
+    //inum integer declared explicit so specification overrides,so inum gets converted to float.
+    testfunc<float, int>(inum);
+    return 0;
+}
