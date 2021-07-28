26f8e773f4b609bed7f746e9b7a0e16b9760aac8
Sanjish Maharjan <sanjishmaharjan619@gmail.com>
2021-07-28 09:42:02 +0545

    Fibonacci series series using post operator overloading

---
+++ Assignments/Lab_Sheet5_questions/Q1_b_fibonacci_post.cpp
@@ -0,0 +1,36 @@
+// Write a program to generate Fibonacci series using operator overloading of ++ operator
+// b) for post increment
+#include <iostream>
+using namespace std;
+class Fibonacci
+{
+    int a = 0, b = 1, c = 0;
+
+public:
+    void show()
+    {
+        cout << c << " ";
+    }
+    void operator++(int)
+    {
+        a = b;
+        b = c;
+        c = a + b;
+    }
+};
+
+int main()
+{
+    int n, i;
+    Fibonacci f1;
+
+    cout << "Please Enter the limit of the series: ";
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        f1.show();
+        f1++;
+    }
+
+    return 0;
+}
