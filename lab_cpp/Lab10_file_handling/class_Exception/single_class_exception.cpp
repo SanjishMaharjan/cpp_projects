28aaf18af2916ad8bf80354f2d9ca59fd5f8bd22
Sanjish Maharjan <sanjishmaharjan619@gmail.com>
2021-07-31 17:02:55 +0545

    single_class_exception

---
+++ lab_cpp/Lab10_file_handling/class_Exception/single_class_exception.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class demo
+{
+};
+int main()
+{
+    try
+    {
+
+        throw demo();
+    }
+    catch (demo d)
+    {
+        cout << "Caught exception of demo class" << endl;
+    }
+    return 0;
+}
+/*
+->In the program we have decalred  an empty class
+->In the try block we are throwing an object of demo class type.
+->The catch block catches the object and displays.
+*/
